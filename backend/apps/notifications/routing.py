71ec49540d3bafe0725ded22660a04d4abcbb52b
Khanh Duy <lenguyenkhanhduy9a2nqs@gmail.com>
2026-07-14 13:41:15 +0700

    fix: accept both /ws/notifications/ and /ws/ws/notifications/ paths

--- backend/apps/notifications/routing.py
+++ backend/apps/notifications/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/notifications/$", consumers.NotificationConsumer.as_asgi()),
+    re_path(r"^ws/notifications/$", consumers.NotificationConsumer.as_asgi()),
+    re_path(r"^ws/ws/notifications/$", consumers.NotificationConsumer.as_asgi()),
 ]
