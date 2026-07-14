a5b18d2cbda4d05d8d2c371b1e57b5d604a01ca4
Khanh Duy <lenguyenkhanhduy9a2nqs@gmail.com>
2026-07-14 13:13:30 +0700

    fix: use REFRESH_TOKEN_LIFETIME from settings instead of hardcoded 7 days

--- backend/apps/users/utils/cookies.py
+++ backend/apps/users/utils/cookies.py
@@ -4,13 +4,18 @@ REFRESH_COOKIE_NAME = "refresh_token"
 
 
 def set_refresh_cookie(response, refresh_token):
+    # Lấy thời gian sống từ settings (REFRESH_TOKEN_LIFETIME) hoặc mặc định 1 ngày
+    from rest_framework_simplejwt.settings import api_settings as jwt_settings
+    refresh_lifetime = jwt_settings.REFRESH_TOKEN_LIFETIME
+    max_age_seconds = int(refresh_lifetime.total_seconds())
+
     response.set_cookie(
         key=REFRESH_COOKIE_NAME,
         value=refresh_token,
         httponly=True,
         secure=not settings.DEBUG,
         samesite="None" if not settings.DEBUG else "Lax",
-        max_age=7 * 24 * 60 * 60,
+        max_age=max_age_seconds,
         path="/",
         domain=None,
     )
